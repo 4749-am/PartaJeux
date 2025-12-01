10b812642b33a627a9b33f40dd6afc492d15ae2f
anne-marie <amm47home@gmail.com>
2025-12-01 16:01:35 +0100

    user dashboard layout enhanced

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -16,25 +16,15 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
+    
     #[Route('/user', name: 'user_dashboard')]
-    public function index(
+    public function dashboard(
         Request $request,
-        JeuRepository $jeuRepository,
-        UserRepository $userRepo,
         EntityManagerInterface $entityManager
     ): Response {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
         $user = $this->getUser();
-        $utilisateurs = $userRepo->findAll();
-
-        $mesJeux = $jeuRepository->findBy(['user' => $user]);
-        $autresJeux = $jeuRepository->createQueryBuilder('j')
-            ->where('j.user != :user')
-            ->setParameter('user', $user)
-            ->orderBy('j.dateSoiree', 'ASC')
-            ->getQuery()
-            ->getResult();
-
+        
         $jeu = new Jeu();
         $form = $this->createForm(JeuType::class, $jeu);
         $form->handleRequest($request);
@@ -44,18 +34,65 @@ class UserController extends AbstractController
             $jeu->addParticipant($user);
             $entityManager->persist($jeu);
             $entityManager->flush();
+            
+            $this->addFlash('success', 'Votre jeu a été créé avec succès !');
 
             return $this->redirectToRoute('user_dashboard');
         }
 
-        return $this->render('user/dashboard.html.twig', [
+        return $this->render('user/user_dashboard.html.twig', [
             'form' => $form->createView(),
-            'mesJeux' => $mesJeux,
-            'autresJeux' => $autresJeux,
-            'utilisateurs' => $utilisateurs
         ]);
     }
 
+    
+    #[Route('/user/mes-jeux', name: 'user_games')]
+    public function userGames(JeuRepository $jeuRepository): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $user = $this->getUser();
+        
+        $mesJeux = $jeuRepository->findBy(['user' => $user], ['dateSoiree' => 'ASC']);
+
+        return $this->render('user/user_games.html.twig', [
+            'jeux_crees' => $mesJeux,
+        ]);
+    }
+
+    
+    #[Route('/user/jeux-disponibles', name: 'available_games')]
+    public function availableGames(JeuRepository $jeuRepository): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $user = $this->getUser();
+        
+        $autresJeux = $jeuRepository->createQueryBuilder('j')
+            ->where('j.user != :user')
+            ->setParameter('user', $user)
+            ->orderBy('j.dateSoiree', 'ASC')
+            ->getQuery()
+            ->getResult();
+
+        return $this->render('user/available_games.html.twig', [
+            'autresJeux' => $autresJeux,
+        ]);
+    }
+
+    
+    #[Route('/user/liste', name: 'user_list')]
+    public function userList(UserRepository $userRepo): Response
+    {
+        
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        $utilisateurs = $userRepo->findAll();
+
+        return $this->render('user/user_list.html.twig', [
+            'utilisateurs' => $utilisateurs,
+        ]);
+    }
+
+    
     #[Route('/user/ban/{id}', name: 'user_ban')]
     public function banUser(
         User $userToBan,
@@ -64,18 +101,18 @@ class UserController extends AbstractController
     ): Response {
         $currentUser = $this->getUser();
 
-        if (!$currentUser) {
+        if (!$currentUser) { 
             throw $this->createAccessDeniedException();
         }
 
         if (in_array('ROLE_ADMIN', $userToBan->getRoles())) {
             $this->addFlash('error', "Impossible de bannir un admin.");
-            return $this->redirectToRoute('user_dashboard');
+            return $this->redirectToRoute('user_list');
         }
 
         if ($userToBan === $currentUser) {
             $this->addFlash('error', "Tu ne peux pas te bannir toi-même.");
-            return $this->redirectToRoute('user_dashboard');
+            return $this->redirectToRoute('user_list');
         }
 
         $userToBan->setIsBanned(true);
@@ -84,9 +121,10 @@ class UserController extends AbstractController
         $banNotifier->sendBanEmail($userToBan);
 
         $this->addFlash('success', "Utilisateur banni.");
-        return $this->redirectToRoute('user_dashboard');
+        return $this->redirectToRoute('user_list');
     }
 
+   
     #[Route('/user/unban/{id}', name: 'user_unban')]
     public function unbanUser(
         User $userToUnban,
@@ -95,13 +133,13 @@ class UserController extends AbstractController
     ): Response {
         $currentUser = $this->getUser();
 
-        if (!$currentUser) {
+        if (!$currentUser) { 
             throw $this->createAccessDeniedException();
         }
 
         if (in_array('ROLE_ADMIN', $userToUnban->getRoles())) {
             $this->addFlash('error', "Impossible d’agir sur un admin.");
-            return $this->redirectToRoute('user_dashboard');
+            return $this->redirectToRoute('user_list');
         }
 
         $userToUnban->setIsBanned(false);
@@ -110,6 +148,6 @@ class UserController extends AbstractController
         $banNotifier->sendUnbanEmail($userToUnban);
 
         $this->addFlash('success', "Utilisateur débanni.");
-        return $this->redirectToRoute('user_dashboard');
+        return $this->redirectToRoute('user_list');
     }
 }
