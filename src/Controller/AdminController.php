cec7db4e10dcb853253be098e9bfc3c27d9819b5
anne-marie <amm47home@gmail.com>
2025-12-01 17:37:53 +0100

    admin dashboard layout enhanced

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Jeu;
 use App\Entity\User;
 use App\Form\JeuType;
+use App\Repository\JeuRepository; 
 use App\Repository\UserRepository;
 use App\Service\BanNotificationService;
 use Doctrine\ORM\EntityManagerInterface;
@@ -13,37 +14,68 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/admin')] 
 class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'admin_dashboard')]
-    public function index(
+    
+    #[Route('/', name: 'admin_dashboard')]
+    public function dashboard(
         Request $request,
-        EntityManagerInterface $entityManager,
-        UserRepository $userRepository
+        EntityManagerInterface $entityManager
     ): Response {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
 
+        
         $jeu = new Jeu();
         $form = $this->createForm(JeuType::class, $jeu);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            
             $jeu->setUser($this->getUser());
             $entityManager->persist($jeu);
             $entityManager->flush();
+            
+            $this->addFlash('success', 'Nouveau jeu créé avec succès par l\'administrateur.');
 
             return $this->redirectToRoute('admin_dashboard');
         }
 
+        
+        return $this->render('admin/admin_dashboard.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+    
+    
+    #[Route('/utilisateurs', name: 'admin_users_list')]
+    public function listUsers(UserRepository $userRepository): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
         $users = $userRepository->findAll();
 
-        return $this->render('admin/dashboard.html.twig', [
-            'form' => $form->createView(),
-            'users' => $users
+        return $this->render('admin/admin_users_list.html.twig', [
+            'users' => $users,
+        ]);
+    }
+    
+    
+    #[Route('/jeux-crees', name: 'admin_my_games')]
+    public function listMyGames(): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        
+        
+        $jeuxCrees = $this->getUser()->getJeux(); 
+
+        return $this->render('admin/admin_my_games.html.twig', [
+            'jeux_crees' => $jeuxCrees,
         ]);
     }
 
-    #[Route('/admin/ban/{id}', name: 'admin_ban')]
+    
+    #[Route('/ban/{id}', name: 'admin_user_ban')] 
     public function banUser(
         User $userToBan,
         EntityManagerInterface $em,
@@ -53,7 +85,7 @@ class AdminController extends AbstractController
 
         if (in_array('ROLE_ADMIN', $userToBan->getRoles())) {
             $this->addFlash('error', "Impossible de bannir un autre administrateur.");
-            return $this->redirectToRoute('admin_dashboard');
+            return $this->redirectToRoute('admin_users_list'); 
         }
 
         $userToBan->setIsBanned(true);
@@ -62,10 +94,10 @@ class AdminController extends AbstractController
         $banNotifier->sendBanEmail($userToBan);
 
         $this->addFlash('success', "Utilisateur banni.");
-        return $this->redirectToRoute('admin_dashboard');
+        return $this->redirectToRoute('admin_users_list'); 
     }
 
-    #[Route('/admin/unban/{id}', name: 'admin_unban')]
+    #[Route('/unban/{id}', name: 'admin_user_unban')] 
     public function unbanUser(
         User $userToUnban,
         EntityManagerInterface $em,
@@ -79,6 +111,6 @@ class AdminController extends AbstractController
         $banNotifier->sendUnbanEmail($userToUnban);
 
         $this->addFlash('success', "Utilisateur réactivé.");
-        return $this->redirectToRoute('admin_dashboard');
+        return $this->redirectToRoute('admin_users_list'); 
     }
 }
